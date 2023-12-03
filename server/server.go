a26e7f2c4bdabc4f947aef76b534e6c9e829448a
mayhim182 <mayankatnitrkl@gmail.com>
2023-12-03 14:35:41 +0530

    server using go

--- server/server.go
+++ server/server.go
@@ -6,11 +6,44 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found. ", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("Name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name =%s\n", name)
+	fmt.Fprintf(w, "Address=%s\n", address)
+
+}
+
 func main() {
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!")
-	})
+	fileServer := http.FileServer(http.Dir("./static")) //New code
+
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/hello", helloHandler)
+
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Starting server at port")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
