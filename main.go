0d9a1842d75f68b44590bf062a68a7dd37a087e4
mayhim182 <mayankatnitrkl@gmail.com>
2023-12-04 12:48:10 +0530

    code push post scrum

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
-//main.go
+// main.go
 package main
 
 import (
-	"fmt",
+	"fmt"
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/",handler)
+func main() {
+	http.HandleFunc("/", handler)
 	fmt.Println("Server is listening on: 8080...")
-	http.ListenAndServe(":8000",nil)
+	http.ListenAndServe(":8000", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Println(w,"Hello, Go web")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hello, Go web")
 }
-
